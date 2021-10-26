1b9c992317867a200de960315c5a38070bc84449
AronBA <72823328+AronBA@users.noreply.github.com>
2021-10-26 11:52:52 +0200

    fixed Übung 8;

--- W01/Übung 8.php
+++ W01/Übung 8.php
@@ -2,7 +2,7 @@
 $lotto = array();
 for ($i = 1;$i <=6; $i++) {
     $n = rand(1,41);
-    if (array_key_exists($n,$lotto)){
+    if (in_array($n,$lotto)) {
         $i--;
     }
     else {
@@ -10,7 +10,7 @@ for ($i = 1;$i <=6; $i++) {
     }
    }
 
-for ($j = 0; $j <= count($lotto); $j++){
+for ($j = 1; $j <= count($lotto); $j++){
     echo $lotto[$j]," ";
 }
 ?>
